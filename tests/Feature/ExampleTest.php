40dea6ee2a0c4cdce91a5f5728a3c87fee3750aa
dormidosan <metahuargen@gmail.com>
2023-12-09 04:43:39 +0000

    no env dusk change order migrations v6

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,16 +3,27 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_load_patients_index(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/patients');
 
         $response->assertStatus(200);
     }
