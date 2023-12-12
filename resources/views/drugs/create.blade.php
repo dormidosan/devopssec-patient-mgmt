ba9d2cacf218f4d09d06d4750e190a6d75e2a109
dormidosan <metahuargen@gmail.com>
2023-12-12 00:00:37 +0000

    new change

--- resources/views/drugs/create.blade.php
+++ resources/views/drugs/create.blade.php
@@ -15,7 +15,8 @@
         </nav>
 
         <div class="row">
-            <div class="col-md-12 grid-margin stretch-card">
+            <div class="col-md-12 grid-margin stretch-card"
+                 <img width="" height="">
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">Create New Patient</h4>
